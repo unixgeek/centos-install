546c235ecf3a14ddc329fd04c1cb09a49c74f312
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2014-06-15 22:12:13 -0500

    Modifed to work with CentOS 5. These are several years old, so maybe they work, maybe they don't.

--- install_scripts/1-update-rpms.sh
+++ install_scripts/1-update-rpms.sh
@@ -3,8 +3,6 @@
 # Upgrade the system.
 #
 
-cp -f conf/CentOS-Base.repo /etc/yum.repos.d
-
-rpm -ivh rpmforge-release-0.5.3-1.el4.rf.$(uname -i).rpm || exit 1
+rpm -ivh rpmforge-release-0.5.3-1.el5.rf.$(uname -i).rpm || exit 1
 
 yum -y update
